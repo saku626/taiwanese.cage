cf71238bc57717a4254f1e4b90140f85167cee23
saku626 <jonathankaneki312@yahoo.co.jp>
2024-03-30 21:57:32 +0900

    変数の変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: %i[top]
   
   def top
-    @requests = Request.where(category_id: 2)
+    target_category = Category.find_by(name: '日常生活')
+    @requests = Request.where(category_id: target_category.id)  
+    target_category1 = Category.find_by(name: 'ビジネス')
+    @requests1 = Request.where(category_id: target_category1.id)
   end
 end
