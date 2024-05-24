fcdcd2724355d012d9fa51bbbb66371de627346f
saku626 <jonathankaneki312@yahoo.co.jp>
2024-05-24 23:54:21 +0900

    1日一回ログイン機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,7 @@
 class TasksController < ApplicationController
-  def index; end
+  def index
+    if logged_in? && (current_user.last_login_at.nil? || current_user.last_login_at.to_date != Date.today)
+      flash[:notice] = "今日初めてのログインです！"
+    end
+  end
 end
