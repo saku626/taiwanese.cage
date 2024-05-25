04cce21fea82755add91bc99663f2c4f4779e2e9
saku626 <jonathankaneki312@yahoo.co.jp>
2024-05-25 23:00:59 +0900

    ログイン修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,13 @@
 class TasksController < ApplicationController
   def index
+    @vocabulary = current_user.vocabularies.find_by(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    @word_created_today = @vocabulary.present?
+
     if logged_in? && (current_user.last_login_at.nil? || current_user.last_login_at.to_date != Date.today)
+      @logged_in_today = true
       flash[:notice] = "今日初めてのログインです！"
+    else
+      @logged_in_today = false
     end
   end
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,6 +7,9 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
   
     if @user
+      # ログイン成功時に最終ログイン日時を更新
+      @user.update(last_login_at: Time.current)
+      logged_in_today = true
       redirect_to requests_path, success: "ログインしました"
     else
       flash.now[:danger] = "ログインに失敗しました"
