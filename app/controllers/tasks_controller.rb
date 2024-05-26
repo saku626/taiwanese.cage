ef70f320a59b939bd4622f61cefeb8517527cfcf
saku626 <jonathankaneki312@yahoo.co.jp>
2024-05-26 23:38:24 +0900

    1日1回単語を作成する条件分岐修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @vocabulary = current_user.vocabularies.find_by(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
-    @word_created_today = @vocabulary.present?
-
     # Date.todayをUTCに合わせる
     today_utc = Time.zone.today
+
+    @vocabulary = current_user.vocabularies.find_by(created_at: today_utc.beginning_of_day..today_utc.end_of_day)
+    @word_created_today = @vocabulary.present?
 
     if logged_in? && current_user.last_login_at.present? && current_user.last_login_at.to_date == today_utc
       @logged_in_today = true
