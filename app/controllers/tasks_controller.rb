3d72e10106bfdc7b0489892282d0fcaa06819a1f
saku626 <jonathankaneki312@yahoo.co.jp>
2024-05-28 23:42:24 +0900

    utcを日本時間に変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
   def index
     # Date.todayをUTCに合わせる
-    today_utc = Time.zone.today
+    today_jst = Time.zone.today
 
-    @word_created_today = current_user.last_vocabulary_created_at.present? && current_user.last_vocabulary_created_at.to_date == today_utc
+    @word_created_today = current_user.last_vocabulary_created_at.present? && current_user.last_vocabulary_created_at.to_date == today_jst
 
-    if logged_in? && current_user.last_login_at.present? && current_user.last_login_at.to_date == today_utc
+    if logged_in? && current_user.last_login_at.present? && current_user.last_login_at.to_date == today_jst
       @logged_in_today = true
       flash[:notice] = "今日初めてのログインです！"
     else
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
   
     if @user
       # ログイン成功時に最終ログイン日時を更新
-      @user.update(last_login_at: Time.current)
+      @user.update(last_login_at: Time.zone.now)
       redirect_to requests_path, success: "ログインしました"
     else
       flash.now[:danger] = "ログインに失敗しました"
