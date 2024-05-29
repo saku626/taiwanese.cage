9eaa229c79999facdd5a386e58ebcac278ca6950
saku626 <jonathankaneki312@yahoo.co.jp>
2024-05-29 23:47:04 +0900

    カレンダーに印をつける

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,13 +4,8 @@ class TasksController < ApplicationController
     today_jst = Time.zone.today
 
     @word_created_today = current_user.last_vocabulary_created_at.present? && current_user.last_vocabulary_created_at.to_date == today_jst
+    @logged_in_today = logged_in? && current_user.last_login_at.present? && current_user.last_login_at.to_date == today_jst
 
-    if logged_in? && current_user.last_login_at.present? && current_user.last_login_at.to_date == today_jst
-      @logged_in_today = true
-      flash[:notice] = "今日初めてのログインです！"
-    else
-      @logged_in_today = false
-      flash[:notice] = "今日"
-    end
+    @both_conditions_met = @logged_in_today && @word_created_today
   end
 end
