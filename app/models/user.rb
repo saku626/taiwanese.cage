0ff99de3fd116629038470d2b524a402868a05d3
saku626 <jonathankaneki312@yahoo.co.jp>
2024-09-14 21:39:08 +0900

    パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, format: {
+  with: /\A(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*\d)|(?=.*[a-z])(?=.*[@$!%*?&])|(?=.*[A-Z])(?=.*\d)|(?=.*[A-Z])(?=.*[@$!%*?&])|(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}\z/,
+  message: "は8文字から32文字で、英小文字、英大文字、数字、およびスペース以外の記号から2種以上を含む必要があります。",
+  if: -> { new_record? || changes[:crypted_password] }
+  }
+
+  validates :password, exclusion: { in: ->(user) { [user.email, user.email.split('@').first, user.email.split('@').last] }, message: "にはメールアドレスを含めることはできません。" }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :name, presence: true, length: { maximum: 255 }
@@ -9,7 +15,7 @@ class User < ApplicationRecord
   validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
 
   has_many :bookmarks, dependent: :destroy
-  has_many :bookmark_requests, through: :bookmarks, source: :request
+  has_many :bookmarked_defaults, through: :bookmarks, source: :default_vocabulary
   has_many :categories, dependent: :destroy
   has_many :vocabularies, dependent: :destroy
   has_many :task_completions, dependent: :destroy
@@ -18,19 +24,19 @@ class User < ApplicationRecord
     self.id == object.user_id #selfは省略できる
   end
 
-  def bookmark(request)
-    bookmark_requests << request
+  def bookmark(default_vocabulary)
+    bookmarked_defaults << default_vocabulary
   end
 
-  def unbookmark(request)
-    bookmark_requests.destroy(request)
+  def unbookmark(default_vocabulary)
+    bookmarked_defaults.destroy(default_vocabulary)
   end
 
-  def bookmark?(request)
-    bookmark_requests.include?(request)
+  def bookmark?(default_vocabulary)
+    bookmarked_defaults.include?(default_vocabulary)
   end
 
-  def bookmark(request, category_id)
-    bookmarks.create(request: request, category_id: category_id)
+  def bookmark(default_vocabulary, category_id)
+    bookmarks.create(default_vocabulary: default_vocabulary, category_id: category_id)
   end
 end
