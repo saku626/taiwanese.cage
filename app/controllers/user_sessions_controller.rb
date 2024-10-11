cc97a9c51019b088ba80f3f92d9af3a115c406f7
saku626 <jonathankaneki312@yahoo.co.jp>
2024-10-12 00:18:25 +0900

    last_login_atの記述削除

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
   
     if @user
       # ログイン成功時に最終ログイン日時を更新
-      @user.update(last_login_at: Time.zone.now)
+      # @user.update(last_login_at: Time.zone.now)
       redirect_to default_vocabularies_path, success: "ログインしました"
     else
       flash.now[:danger] = "ログインに失敗しました"
